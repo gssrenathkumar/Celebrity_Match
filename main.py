28b23815fbba63cf03817a1a161e49a7f8b336ce
gssrenathkumar <srenathindustries215@gmail.com>
2024-01-02 00:37:39 +0530

    Data Ingestion Task Completed

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-from celebrity_match.logging import logger
-logger.info("Welcome to our custom logging")
+
+from src.celebrity_match.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.celebrity_match.logging import logger
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx===========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
